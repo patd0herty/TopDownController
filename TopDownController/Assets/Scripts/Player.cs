86f611b8efde4e6c904c0da7586819e1d8fb9d89
Patrick Doherty <prd9410@rit.edu>
2025-02-11 00:56:43 -0500

    Added Walking animation and corresponding logic

--- TopDownController/Assets/Scripts/Player.cs
+++ TopDownController/Assets/Scripts/Player.cs
@@ -10,12 +10,27 @@ public class Player : MonoBehaviour
     [SerializeField] private Rigidbody2D _rb;
 
     private Vector2 _moveDirect = Vector2.zero;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private Animator _animator;
     #endregion
 
-    #region Tick
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponentInChildren<Animator>();
+        _animator.SetFloat("xVelocity", 0.0f);
+        _animator.SetFloat("yVelocity", 0.0f);
+        _animator.SetBool("xEqualZero", true);
+        _animator.SetBool("yEqualZero", true);
+    }
+
+    #region Loop
     private void Update()
     {
         GatherInput();
+        _walkAnimation();
     }
 
     private void FixedUpdate()
@@ -43,5 +58,38 @@ public class Player : MonoBehaviour
         
     }
 
+    #endregion
+
+    #region HelperMethods
+    private void _walkAnimation()
+    {
+        _animator.SetFloat("xVelocity", _moveDirect.x);
+        _animator.SetFloat("yVelocity", _moveDirect.y);
+        // Check if X direction is exactly zero and update animator bools accordingly
+        if(_moveDirect.x == 0)
+        {
+            _animator.SetBool("xEqualZero", true);
+        }
+        else
+        {
+            _animator.SetBool("xEqualZero", false);
+        }
+        // Check if y direction is exactly zero and update animator bools accordingly
+        if (_moveDirect.y == 0)
+        {
+            _animator.SetBool("yEqualZero", true);
+        }
+        else
+        {
+            _animator.SetBool("yEqualZero", false);
+        }
+    }
+
+    private string _getDirection()
+    {
+
+        // if something goes wrong default to the down animationn
+        return "down";
+    }
     #endregion
 }
